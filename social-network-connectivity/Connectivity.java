64f3725085ab042e9e6432fd7fca9bcdb01c4d16
Ino Xu <inoxu@192.168.1.180>
2026-06-08 21:08:27 +0100

    herro

---
+++ social-network-connectivity/Connectivity.java
@@ -0,0 +1,11 @@
+public class Connectivity {
+    public static void main(String[] args) {
+        int numArgs = args.length;
+        System.out.println("Herro");
+        System.out.println(numArgs);
+        for(int i = 0; i < numArgs; i++) {
+            String arg = args[i];
+            System.out.println(arg);
+        }
+    }
+}
